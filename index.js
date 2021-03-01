00632342cfc7889d77e2682dd8fab08dcf3de173
Tildoss <thimothe.g@gmail.com>
2021-03-01 21:07:50 +0100

    Securisation du token du bot

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const Wiki = require('./wiki/wiki');
+const { token } = require('./config.json');
 
 bot.on('message' , function (message) {
     let commandUsed = Wiki.parse(message); //Utiliser || pour les autres commandes
 });
 
-bot.login('ODE1Mjg0NDgwNzY3Njg4NzE0.YDqK1w.rkkqQ5ymWKckFmZOsAbvhY83ohI');
+bot.login(token);
