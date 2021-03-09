493c0c8cd3fc790b391009b21bc39e59ff1850be
Tildoss <thimothe.g@gmail.com>
2021-03-09 11:35:56 +0100

    MaJ des requetes de AJAX vers Axios

--- wiki/wiki.js
+++ wiki/wiki.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios').default;
 const Command = require('./command')
 
 module.exports =  class wiki extends Command {
@@ -17,62 +19,28 @@ module.exports =  class wiki extends Command {
         let args = [message.content.slice(0,i), message.content.slice(i+1)];
         args.shift();
 
-        //On init la requete
-        var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-        var request = new XMLHttpRequest();
+        axios.get('https://fr.wikipedia.org/w/api.php?action=opensearch&search=' + args)
+            .then((response) => {
+                var pages = response.data["3"]["0"];
+                var finUrl = pages.split('/');
+                var nomPages = finUrl.pop();
 
-        //On lance la requete
-        request.open("GET", "https://fr.wikipedia.org/w/api.php?action=opensearch&search=" + args);
-        request.send();
-        
-        request.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var response = JSON.parse(this.responseText);
-                var pages = response["3"]["0"];
-                var tabPages = pages.split("/");
-                //on récupère le dernier élément de l'url
-                var nomPages = tabPages.pop();
-
-                var request2 = new XMLHttpRequest();
-                request2.open("GET", "https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=redirects&redirects&titles=" + nomPages);
-                request2.send();
-
-                request2.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                        var response2 = JSON.parse(this.responseText);
-                        var pages2 = response2["query"]["pages"];
-                        for (var id in pages2) {
-                            if (pages2.hasOwnProperty(id)) {
-                                var idpage = pages2[id]["pageid"];
+                axios.get('https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=redirects&redirects&titles=' + nomPages)
+                    .then((response) => {
+                        pages = response.data["query"]["pages"];
+                        for (var id in pages) {
+                            if (pages.hasOwnProperty(id)) {
+                                var idPage = pages[id]["pageid"];
                             };
                         };
-                        var request3 = new XMLHttpRequest();
-                        request3.open("GET", "https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=true&explaintext&redirects=1&pageids=" + idpage);
-                        request3.send();
 
-                        request3.onreadystatechange = function () {
-                            if (this.readyState == 4 && this.status == 200) {
-                                //On parse le json obtenu
-                                var response3 = JSON.parse(this.responseText);
-                                var pages3 = response3["query"]["pages"];
-
-                                //On boucle pour déterminer l'id de la page
-                                for (var id in pages3) {
-                                    if (pages3.hasOwnProperty(id)) {
-                                        var resume = pages3[id];
-                                    };
-                                };
-
-                                // var tab = resume["extract"].split('\n').forEach((item) => {
-                                //     message.channel.send(item);
-                                // });
-                                var para = resume["extract"].split('\n');
+                        axios.get('https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=true&explaintext&redirects=1&pageids=' + idPage)
+                            .then((response) => {
+                                var resume = response.data["query"]["pages"][idPage]["extract"];
+                                var para = resume.split('\n');
                                 message.channel.send(para[0]);
-                            };
-                        };
-                    };
-                };
-            };
-        };
+                            })
+                    })
+            })
     };
 };
