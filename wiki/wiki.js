74f0be433cf2bd0341082cd5559f8b2464e5ccc9
Tildoss <thimothe.g@gmail.com>
2021-03-14 11:12:05 +0100

    gestion des réactions qui ne marchent pas

--- wiki/wiki.js
+++ wiki/wiki.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, User } = require('discord.js');
 const { MessageReaction } = require('discord.js');
 const axios = require('axios').default;
 const Command = require('./command')
@@ -25,35 +25,80 @@ module.exports = class wiki extends Command {
         axios.get('https://fr.wikipedia.org/w/api.php?action=opensearch&search=' + encoded)
             .then((response) => {
                 //On récup la fin de l'url
-                var pages = response.data["3"]["0"];
-                var finUrl = pages.split('/');
-                var nomPages = finUrl.pop();
+                let pages = response.data["3"]["0"];
+                let finUrl = pages.split('/');
+                let nomPages = finUrl.pop();
 
                 axios.get('https://fr.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&redirects&titles=' + nomPages)
                     .then((response) => {
                         //On recup l'id de la page associé
-                        var idPage = response.data["query"]["pages"]["0"]["pageid"];
+                        const idPage = response.data["query"]["pages"]["0"]["pageid"];
 
                         axios.get('https://fr.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=true&explaintext&redirects=1&pageids=' + idPage)
                             .then((response) => {
                                 //On recup le titre et le resume de la page
-                                var titre = response.data["query"]["pages"]["0"]["title"];
-                                var resume = response.data["query"]["pages"]["0"]["extract"];
-                                var para = resume.split('\n');
+                                const titre = response.data["query"]["pages"]["0"]["title"];
+                                const resume = response.data["query"]["pages"]["0"]["extract"];
+                                const para = resume.split('\n');
 
                                 axios.get('https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=pageimages&pageids=' + idPage)
                                     .then((response) => {
                                         //On recup la miniature
-                                        var thumbnail = response.data["query"]["pages"][idPage]["thumbnail"]["source"];
+                                        const thumbnail = response.data["query"]["pages"][idPage]["thumbnail"]["source"];
                                         //On crée le message embed
+                                        //compteur pour afficher para[cpt]
+                                        //let cpt = 0;
                                         let embed = new MessageEmbed()
                                             .setTitle(decodeURI(nomPages.split('_').join(' ')))
                                             .setURL('https://fr.wikipedia.org/wiki/' + titre.split(' ').join('_'))
                                             .setThumbnail(thumbnail)
                                             .setColor(0xffd801)
                                             .setDescription(para[0]);
-                                        message.channel.send(embed).catch((error) => {
-                                            message.channel.send('Erreur : page vide');
+                                        message.channel.send(embed).then((message) => {
+                                            message.react('◀️').then(() =>
+                                                message.react('▶️')).then(() => {
+                                                    const filter = (reaction) => {
+                                                        return ['◀️', '▶️'].includes(reaction.emoji.name);
+                                                    };
+                                                    console.log(filter);
+                                                    message.awaitReactions(filter, { max: 10, time: 60000, errors: ['time'] }).then(collected => {
+                                                        const reaction = collected.first();
+                                                        let cpt = 0;
+                                                        //console.log(reaction);
+                                                        //console.log(collected);
+                                                        //console.log(collected.first());
+                                                        if (collected.first().emoji.name === '◀️' && cpt > 0) {
+                                                            cpt--;
+                                                            let editEmbed = new MessageEmbed()
+                                                                .setTitle(decodeURI(nomPages.split('_').join(' ')))
+                                                                .setURL('https://fr.wikipedia.org/wiki/' + titre.split(' ').join('_'))
+                                                                .setThumbnail(thumbnail)
+                                                                .setColor(0xffd801)
+                                                                .setDescription(para[cpt]);
+                                                            message.edit(editEmbed);
+                                                        };
+                                                        if (collected.first().emoji.name === '▶️' && cpt < para.length) {
+                                                            cpt++;
+                                                            let editEmbed = new MessageEmbed()
+                                                                .setTitle(decodeURI(nomPages.split('_').join(' ')))
+                                                                .setURL('https://fr.wikipedia.org/wiki/' + titre.split(' ').join('_'))
+                                                                .setThumbnail(thumbnail)
+                                                                .setColor(0xffd801)
+                                                                .setDescription(para[cpt]);
+                                                            message.edit(editEmbed);
+                                                        };
+                                                    })
+                                                        .catch((error) => {
+                                                            message.channel.send('grosse folle');
+                                                            //console.log(error);
+                                                        });
+                                                })
+                                                .catch((error) => {
+                                                    console.log('Erreur reaction');
+                                                    console.log(error);
+                                                });
+                                        }).catch((error) => {
+                                            message.channel.send('Erreur : page inexistante, changez votre requête');
                                             console.log(error);
                                         });
                                     });
