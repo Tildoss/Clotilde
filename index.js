fdf4c83d01e01cfe352c5439a51fbfdce6a6ab2b
Tildoss <thimothe.g@gmail.com>
2021-04-01 14:43:46 +0200

    Update des directory et message d'aide

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const Wiki = require('./wiki/wiki');
-const Autist = require('./autist/autist');
+const Wiki = require('./commande/wiki');
+const Autist = require('./commande/autist');
+const Help = require('./commande/help')
 const { token } = require('./config.json');
 
+bot.on('ready', () => {
+    bot.user.setActivity('*help', { type: "PLAYING" }).catch(console.error)
+})
+
 bot.on('message' , function (message) {
-    let commandUsed = Wiki.parse(message) || Autist.parse(message); //Utiliser || pour les autres commandes
+    let commandUsed = Wiki.parse(message) || Autist.parse(message) || Help.parse(message); //Utiliser || pour les autres commandes
 });
 
 bot.login(token);
